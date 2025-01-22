4c32e9380632a1caf036af9e5416fa4214ac7e69
jon-seana <e1122894@u.nus.edu>
2025-01-23 01:30:18 +0800

    Added Add and List functionalities

--- src/main/java/Botzilla.java
+++ src/main/java/Botzilla.java
@@ -8,21 +8,33 @@ public class Botzilla {
         System.out.println("\t_____________________________________________________________________");
 
         Scanner scanner = new Scanner(System.in);
-
+        String[] list = new String[100];
+        int a = 0;
 
         while (true) {
             String input = scanner.nextLine();
 
-            if (!input.equals("bye")) {
+            if (input.equals("list")) {
                 System.out.println("\t_____________________________________________________________________");
-                System.out.println("\t " + input);
+                int lengthOfList = list.length;
+                for (int i = 0; i < lengthOfList; i++) {
+                    if (list[i] != null) {
+                        int b = i + 1;
+                        System.out.println("\t " + b + ". " + list[i]);
+                    }
+                }
                 System.out.println("\t_____________________________________________________________________");
-                System.out.println(" ");
-            } else {
+            } else if (input.equals("bye")) {
                 System.out.println("\t_____________________________________________________________________");
                 System.out.println("\t Bye. Hope to see you again soon!");
                 System.out.println("\t_____________________________________________________________________");
                 break;
+            } else {
+                list[a++] = input;
+                System.out.println("\t_____________________________________________________________________");
+                System.out.println("\t " + "added: " + input);
+                System.out.println("\t_____________________________________________________________________");
+                System.out.println(" ");
             }
         }
 
