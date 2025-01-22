ef91ac40bd2f2aeebf841babcf2ef7ea1b5151c0
jon-seana <e1122894@u.nus.edu>
2025-01-23 00:47:20 +0800

    add Botzilla.java

---
+++ src/main/java/Botzilla.java
@@ -0,0 +1,11 @@
+public class Botzilla {
+    public static void main(String[] args) {
+        System.out.println("_____________________________________________________________________");
+        System.out.println("Hello! I'm Botzilla");
+        System.out.println("What can I do for you?");
+        System.out.println("_____________________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_____________________________________________________________________");
+    }
+}
+
