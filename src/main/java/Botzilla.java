66012bd2661a46687406f2bb00588802e5a646e6
jon-seana <e1122894@u.nus.edu>
2025-01-23 01:09:26 +0800

    Added Echo functionality

--- src/main/java/Botzilla.java
+++ src/main/java/Botzilla.java
@@ -1,11 +1,32 @@
+import java.util.Scanner;
+
 public class Botzilla {
     public static void main(String[] args) {
-        System.out.println("_____________________________________________________________________");
-        System.out.println("Hello! I'm Botzilla");
-        System.out.println("What can I do for you?");
-        System.out.println("_____________________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("_____________________________________________________________________");
+        System.out.println("\t_____________________________________________________________________");
+        System.out.println("\t Hello! I'm Botzilla");
+        System.out.println("\t What can I do for you?");
+        System.out.println("\t_____________________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (!input.equals("bye")) {
+                System.out.println("\t_____________________________________________________________________");
+                System.out.println("\t " + input);
+                System.out.println("\t_____________________________________________________________________");
+                System.out.println(" ");
+            } else {
+                System.out.println("\t_____________________________________________________________________");
+                System.out.println("\t Bye. Hope to see you again soon!");
+                System.out.println("\t_____________________________________________________________________");
+                break;
+            }
+        }
+
+        scanner.close();
     }
 }
 
